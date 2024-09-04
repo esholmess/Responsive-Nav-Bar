dffd0043af6b5faf86b4156109cd26f610b6af20
esholmess <excelsiorr221@gmail.com>
2024-09-04 13:40:51 +0300

    new design

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function showSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.right = "0"
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.right = "0";
+    document.addEventListener('click', closeSidebarOnClickOutside);
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.right= "-100%"
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.right = "-200%";
+    document.removeEventListener('click', closeSidebarOnClickOutside);
+}
+
+function closeSidebarOnClickOutside(event) {
+    const sidebar = document.querySelector(".sidebar");
+    const menuButton = document.querySelector(".menubutton");
+
+    // Eğer tıklanan alan menünün veya menü butonunun dışında ise menüyü kapatır
+    if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+        hideSidebar();
+    }
 }
